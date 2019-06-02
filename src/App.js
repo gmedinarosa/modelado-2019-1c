6893526157a7a1688b63be2838e84bc0af484a43
Gonza <gonzalo.medinarosa@litebyte.us>
2019-06-02 15:26:33 -0300

    Move chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import MathQuill, {addStyles as addMathquillStyles} from 'react-mathquill'
 import Typography from '@material-ui/core/Typography'
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend} from 'recharts'
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts'
 import mqToMathJS from './utils/mqToMathJS'
 import * as math from 'mathjs'
 
@@ -10,125 +10,131 @@ addMathquillStyles()
 
 class App extends React.Component {
 
-	constructor(props) {
-		super(props)
-		this.state = {
-			latex: 'x^2',
-			exp: 'x^2',
-			data: [],
-			aproxData: [],
-			parseError: false,
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      latex: 'x^2',
+      exp: 'x^2',
+      data: [],
+      aproxData: [],
+      parseError: false,
+    }
+  }
 
-	calcFn(exp) {
-		return new Promise((resolve) => {
-			let data = []
-			let parseError = false
-			try {
-				for (let x = -5; x <= 5; x++) {
-					const y = math.eval(exp, {x})
-					if (typeof y !== 'undefined' && typeof y !== 'object') {
-						data.push({
-							'name': x,
-							'y': y,
-						})
-					}
-				}
-			} catch (err) {
-				data = this.state.data
-				parseError = true
-				console.warn('Parse error')
-			}
-			this.setState({data, parseError})
-			resolve(parseError)
-		})
-	}
+  calcFn(exp) {
+    return new Promise((resolve) => {
+      let data = []
+      let parseError = false
+      try {
+        for (let x = -5; x <= 5; x++) {
+          const y = math.eval(exp, {x})
+          if (typeof y !== 'undefined' && typeof y !== 'object') {
+            data.push({
+              'name': x,
+              'y': y,
+            })
+          }
+        }
+      } catch (err) {
+        data = this.state.data
+        parseError = true
+        console.warn('Parse error')
+      }
+      this.setState({data, parseError})
+      resolve(parseError)
+    })
+  }
 
-	calcAproxFn(exp, h) {
-		return new Promise((resolve) => {
+  calcAproxFn(exp, h) {
+    return new Promise((resolve) => {
 
-			const fn = (x) => math.eval(exp, {x})
-			const fnXb = (Xa, Ta, h) => Xa + fn(Ta) * h
+      const fn = (x) => math.eval(exp, {x})
+      const fnXb = (Xa, Ta, h) => Xa + fn(Ta) * h
 
-			let Ta = 0
-			let Xa = 0
+      let Ta = 0
+      let Xa = 0
 
-			let aproxData = [{'name': Ta, 'y': Xa}]
+      let aproxData = [{'name': Ta, 'y': Xa}]
 
-			try {
-				for (let i = 1; i <= 5; i++) {
-					let Tb = i * h
-					let Xb = fnXb(Xa, Ta, h)
-					console.log(Tb, Xb)
-					if (typeof Xb !== 'undefined' && typeof Xb !== 'object') {
-						aproxData.push({
-							'name': Tb,
-							'y': Xb,
-						})
-					}
+      try {
+        for (let i = 1; i <= 5; i++) {
+          let Tb = i * h
+          let Xb = fnXb(Xa, Ta, h)
+          console.log(Tb, Xb)
+          if (typeof Xb !== 'undefined' && typeof Xb !== 'object') {
+            aproxData.push({
+              'name': Tb,
+              'y': Xb,
+            })
+          }
 
-					Ta = Tb
-					Xa = Xb
-				}
-			} catch (err) {
-				aproxData = this.state.aproxData
-				console.warn('Parse error')
-			}
-			this.setState({aproxData})
-			resolve()
-		})
-	}
+          Ta = Tb
+          Xa = Xb
+        }
+      } catch (err) {
+        aproxData = this.state.aproxData
+        console.warn('Parse error')
+      }
+      this.setState({aproxData})
+      resolve()
+    })
+  }
 
-	render() {
+  render() {
 
-		return (
-			<div style={{display: 'flex', width: '100%', height: '100%'}}>
-				<div style={{flex: 1}}>
-					<Typography variant="h3" gutterBottom>Sistemas autónomos 1D</Typography>
-					<div style={{display: 'flex'}}>
-						<Typography variant="body1" style={{alignSelf: 'center'}}>x' = </Typography>
-						<MathQuill
-							style={{color: 'red'}}
-							latex={this.state.latex}
-							onChange={latex => {
-								const exp = mqToMathJS(latex)
-								console.log(exp, latex)
-								this.calcFn(exp)
-								this.calcAproxFn('2x', 0.5)
-								this.setState({latex, exp})
-							}}
-						/>
-					</div>
-					<div style={{display: 'flex'}}>
-						{this.state.parseError? 'parse error' : null}
-					</div>
-				</div>
-				<div style={{flex: 1}}>
-					<LineChart width={730} height={250} data={this.state.data}
-										 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-						<CartesianGrid strokeDasharray="3 3"/>
-						<XAxis dataKey="name" domain={[-5, 5]} scale={'linear'} type={'number'} allowDataOverflow={true}/>
-						<YAxis domain={[-5, 5]} scale={'linear'} allowDataOverflow={true}/>
-						<Tooltip/>
-						<Legend/>
-						<Line type="linear" dataKey="y" stroke="#82ca9d"/>
-					</LineChart>
-				</div>
-				<div style={{flex: 1}}>
-					<LineChart width={730} height={250} data={this.state.aproxData}
-										 margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-						<CartesianGrid strokeDasharray="3 3"/>
-						<XAxis dataKey="name" domain={[-5, 5]} scale={'linear'} type={'number'} allowDataOverflow={true}/>
-						<YAxis domain={[-5, 5]} scale={'linear'} allowDataOverflow={true}/>
-						<Tooltip/>
-						<Legend/>
-						<Line type="linear" dataKey="y" stroke="#82ca9d"/>
-					</LineChart>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div style={{display: 'flex', width: '100%', height: '100%'}}>
+        <div style={{flex: 1}}>
+          <Typography variant="h3" gutterBottom>Sistemas autónomos 1D</Typography>
+          <div style={{display: 'flex'}}>
+            <Typography variant="body1" style={{alignSelf: 'center'}}>x' = </Typography>
+            <MathQuill
+              style={{color: 'red'}}
+              latex={this.state.latex}
+              onChange={latex => {
+                const exp = mqToMathJS(latex)
+                console.log(exp, latex)
+                this.calcFn(exp)
+                this.calcAproxFn('2x', 0.5)
+                this.setState({latex, exp})
+              }}
+            />
+          </div>
+          <div style={{display: 'flex'}}>
+            {this.state.parseError ? 'parse error' : null}
+          </div>
+        </div>
+        <div style={{flex: 1, height: '100vh'}}>
+          <div style={{height: '33vh'}}>
+            <ResponsiveContainer>
+              <LineChart data={this.state.data}
+                         margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name" domain={[-5, 5]} scale={'linear'} type={'number'} allowDataOverflow={true}/>
+                <YAxis domain={[-5, 5]} scale={'linear'} allowDataOverflow={true}/>
+                <Tooltip/>
+                <Legend/>
+                <Line type="linear" dataKey="y" stroke="#82ca9d"/>
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <div style={{height: '33vh'}}>
+            <ResponsiveContainer>
+              <LineChart data={this.state.aproxData}
+                         margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <XAxis dataKey="name" domain={[-5, 5]} scale={'linear'} type={'number'} allowDataOverflow={true}/>
+                <YAxis domain={[-5, 5]} scale={'linear'} allowDataOverflow={true}/>
+                <Tooltip/>
+                <Legend/>
+                <Line type="linear" dataKey="y" stroke="#82ca9d"/>
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
