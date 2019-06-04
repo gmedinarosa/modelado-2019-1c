46a3cf00a90d59f8b4c3056fa48e0089c3464aa2
Gonza <gonzalo.medinarosa@litebyte.us>
2019-06-03 21:31:51 -0300

    Prevent unnecessary renders in XvsT

--- src/charts/Xvst.js
+++ src/charts/Xvst.js
@@ -5,7 +5,7 @@ import draw from './d3'
 
 const RD3Component = rd3.Component
 
-class Xvst extends React.PureComponent {
+class Xvst extends React.Component {
 
   static propTypes = {
     data: PropTypes.array,
@@ -20,23 +20,24 @@ class Xvst extends React.PureComponent {
     })
   }
 
-  // shouldComponentUpdate(nextProps, nextState, nextContext) {
-  //
-  // }
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    if (JSON.stringify(nextProps.data) !== JSON.stringify(this.props.data)) return true
+    if (nextProps.density !== this.props.density) return true
+    if (nextProps.xAxis.min !== this.props.xAxis.min) return true
+    if (nextProps.xAxis.max !== this.props.xAxis.max) return true
+    if (nextProps.yAxis.min !== this.props.yAxis.min) return true
+    if (nextProps.yAxis.max !== this.props.yAxis.max) return true
+    return false
+  }
 
   constructor(props) {
     super(props)
     this.state = {d3: ''}
   }
 
-  // componentDidMount() {
-  //   this.setState({d3: node})
-  // }
-
   render() {
     const {data, density, xAxis, yAxis} = this.props
     const node = draw(data, density, xAxis.min, xAxis.max, yAxis.min, yAxis.max)
-    // console.log(node)
     return (
       <div>
         <RD3Component data={node}/>
