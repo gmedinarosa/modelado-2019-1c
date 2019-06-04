2680e0e5ba1d1ec2027bfd740ccf379cce921277
Gonza <gonzalo.medinarosa@litebyte.us>
2019-06-04 10:57:56 -0300

    Draw lines in both directions

--- src/graphers/Approx.js
+++ src/graphers/Approx.js
@@ -6,14 +6,37 @@ function Approx(fn, h, T0, X0, xMin, xMax, yMin, yMax) {
     let Ta = T0
     let Xa = X0
 
-    let data = [{x: Ta, y: Xa}]
+    let fdata = [{x: Ta, y: Xa}]
 
-    for (let i = 1; i <= xMax; i += h) {
+    // Forwards
+    for (let i = 0; i <= xMax - T0; i += h) {
       let Tb = Ta + h
       let Xb = fnXb(Xa, Ta, h)
 
       if (typeof Xb !== 'undefined' && typeof Xb !== 'object') {
-        data.push({x: Tb, y: Xb})
+        fdata.push({x: Tb, y: Xb})
+      }
+
+      // No need to keep going
+      if (Xb > yMax) break
+      if (Xb < yMin) break
+
+      Ta = Tb
+      Xa = Xb
+    }
+
+    Ta = T0
+    Xa = X0
+
+    let bdata = []
+
+    // Backwards
+    for (let i = 0; i >= xMin - T0; i -= h) {
+      let Tb = Ta - h
+      let Xb = fnXb(Xa, Ta, -h)
+
+      if (typeof Xb !== 'undefined' && typeof Xb !== 'object') {
+        bdata.push({x: Tb, y: Xb})
       }
 
       //No need to keep going
@@ -23,6 +46,10 @@ function Approx(fn, h, T0, X0, xMin, xMax, yMin, yMax) {
       Ta = Tb
       Xa = Xb
     }
+
+    let data = bdata.reverse()
+    data = data.concat(...fdata)
+
     resolve(data)
   })
 }
